223f4c1309c260a4d5449596d0cd06a1887ee1de
vishal Suryawanshi <vishalsuryawanshidsp@gmail.com>
2026-09-15 11:04:15 +0530

    Second Commit

---
+++ src/main/java/Testcases/Test3.java
@@ -0,0 +1,17 @@
+package Testcases;
+
+import org.testng.annotations.Test;
+
+public class Test3 {
+	
+	@Test
+	public void runTestOne() {
+		System.out.println("Test 5 executed");
+	}
+	
+	@Test
+	public void runTestTwo() {
+		System.out.println("Test 6 executed");
+	}
+
+}
